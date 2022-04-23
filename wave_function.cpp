cfed835511a32d248e527d22317ea765122fd6e5
Ivan Shibanov <shibanov.ivan@gmail.com>
2022-04-23 10:27:52 -0400

    Set up boilerplate

--- wave_function.cpp
+++ wave_function.cpp
@@ -1,22 +1,49 @@
 #include "wave_function.h"
+#include <random>
+
+namespace
+{
+    std::random_device dev;
+    std::mt19937 rng( dev() );
+}
 
 int WaveTile::countPossibilities() const
 {
-    return 0;
+    int count = 0;
+    int temp = possible;
+    while ( temp ) {
+        count += temp & 1;
+        temp >>= 1;
+    }
+    return count;
 }
 
-void WaveTile::limit( int fromTileType ) {}
+void WaveTile::limit( int fromTileType )
+{
+    // Rules!
+    possible = LAKE1 | LAKE2 | DEEP_WATER;
+}
 
-WaveMap::WaveMap( int side ) {}
+WaveMap::WaveMap( int side )
+{
+    _map.reserve( side * side );
+    _rowSize = side;
+    for ( int i = 0; i < side * side; i++ ) {
+        _map.emplace_back();
+    }
+}
 
 WaveTile & WaveMap::getTile( size_t index )
 {
-    // TODO: insert return statement here
+    return _map[index];
 }
 
 std::vector<size_t> WaveMap::getAdjacent4( size_t index ) const
 {
-    return std::vector<size_t>();
+    std::vector<size_t> retval;
+    retval.reserve( 4 );
+
+    return retval;
 }
 
 std::vector<size_t> WaveMap::getAdjacent8( size_t index ) const
@@ -26,6 +53,14 @@ std::vector<size_t> WaveMap::getAdjacent8( size_t index ) const
 
 bool WaveMap::place( size_t index )
 {
+    if ( index < _map.size() ) {
+        const int possible = _map[index].countPossibilities();
+        if ( possible ) {
+            std::uniform_int_distribution<std::mt19937::result_type> distribution( 1, _map[index].countPossibilities() );
+
+            int result = distribution( rng );
+        }
+    }
     return false;
 }
 
